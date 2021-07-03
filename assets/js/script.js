25c0084fb0541454337692d5987a913b0d938352
Ahmad Fauzy <69064067+madfauzy@users.noreply.github.com>
2021-07-03 18:31:58 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$('.page-scroll').on('click', function(e){
+
+    var tujuan = $(this).attr('href');
+    var elemenTujuan = $(tujuan);
+
+    $('html').animate({
+        scrollTop: elemenTujuan.offset().top - 70
+    }, 1250)
+
+    e.preventDefault();
+})
